28dd6382f2ce30c4aa6830a76a993e7b5d9062d3
Piotr Radziszewski <radnet.makis@o2.pl>
2019-02-03 17:48:29 +0100

    add test2 - websocket

---
+++ test2/ws/client.js
@@ -0,0 +1,133 @@
+var request = require('request');
+var Stomp = require('stompjs');
+
+var wsClient;
+var id;
+var interval;
+var msgCount;
+var timeSpan;
+var delay;
+var countSendMsg = 1;
+var sendMsgReqCount = 0;
+var stopReceive;
+var countRecMsg = 0;
+
+
+process.on('message', function(message) {
+	var msg = JSON.parse(message);
+	if (msg.type === "connect") {
+		id = parseInt(msg.id);
+		msgCount = parseInt(msg.msgCount);
+		timeSpan = parseInt(msg.timeSpan);
+		delay = parseFloat(msg.delay);
+		stopReceive = parseInt(msg.stopReceive);
+		
+		//console.log("client: " + id);
+		//console.log("msgCount: " + msgCount);
+		//console.log("timeSpan: " + timeSpan);
+		//console.log("delay: " + delay);
+
+		connect();
+
+	} 
+	else if (msg.type === "startTest") {
+		startTest();
+	}
+	
+	
+});
+
+function connect() {
+	
+	wsClient = Stomp.overWS('ws://localhost:8080/send');
+	
+	wsClient.connect({}, function(frame) {
+        //console.log('Connected');
+		
+		wsClient.subscribe('/topic/receive', function(message) {
+            var res = JSON.parse(message.body);
+			countRecMsg++;
+		
+			handleResponse(res);
+			
+			if(countRecMsg == stopReceive)
+			{
+				//console.log("client " + id + " end receive");
+				endTest();
+			}
+        });
+		
+		process.send(JSON.stringify({"type": "connected"}));	
+		
+    });
+
+	
+}
+
+function startTest() {
+	//console.log("client " + id + " start test");
+	
+	setTimeout(function() {
+		
+		interval = setInterval(function() {
+			
+			if(sendMsgReqCount < msgCount){
+				sendMsg();
+				sendMsgReqCount++;
+			}
+			else {
+				console.log("Client " + id + " end send msg");
+				clearInterval(interval);
+			}
+			
+		}, timeSpan);
+	}, delay);
+}
+
+
+function sendMsg() {
+	//console.log("client " + id + " send msg: " + countSendMsg);
+
+	var data = {
+		clientID: id,
+		timestamp: Date.now(),
+		payload: ""
+	}
+	wsClient.send('/ws/send', {}, JSON.stringify(data));
+
+}
+
+var times = [];
+
+function handleResponse(res) {
+	//console.log(res);
+	var nowTime = Date.now();
+	times.push(nowTime - res.timestamp);
+	//console.log("client " + id + " " + (nowTime - res.timestamp));
+}
+
+
+function endTest() {
+	
+	var sum = 0;
+	
+	for(var i =0; i<times.length; i++) {
+		
+		sum += times[i];
+	}
+	
+	var avg = sum/times.length;
+	
+	setTimeout(function() {
+		process.send(JSON.stringify({
+			"type": "end",
+			"avg": avg
+			})
+		);
+	}, id*10);
+					
+	//console.log("END AVG: " + avg);
+
+}
+
+
---
+++ test2/ws/master_client.js
@@ -0,0 +1,87 @@
+var cp = require('child_process');
+
+var args = process.argv.slice(2);
+
+var clients = {
+	count: parseInt(args[0]),
+	msgCount: parseInt(args[1]),
+	msgSpan: parseInt(args[2]),
+	list: [],
+	clientsConnected: 0,
+	clientsFinished: 0,
+	avg: []
+};
+
+
+function createClients() {
+	//console.log("countClient: " + clients.count);
+	//console.log("msgCount: " + clients.msgCount);
+	//console.log("msgSpan: " + clients.msgSpan);
+	
+	
+	for (var i = 0; i < clients.count; i++) {
+		var client = cp.fork('./client.js');
+		clients.list.push(client);
+		
+		client.send(JSON.stringify({
+			"type": "connect", 
+			"id": i+1,
+			"timeSpan": clients.msgSpan,
+			"msgCount": clients.msgCount,
+			"delay": (i+1) * (clients.msgSpan / clients.count),
+			"stopReceive": clients.msgCount * ( clients.count - 1)
+		}));
+		
+		client.on('message', function(message) {
+			var msg = JSON.parse(message);
+
+			if (msg.type === 'connected') {
+				clients.clientsConnected++;
+				if (clients.clientsConnected === clients.count) {
+					console.log("All clients connected");
+					startTest();
+				}
+			}
+			else if (msg.type === 'end') {
+				clients.clientsFinished++;
+				clients.avg.push(msg.avg);
+				if (clients.clientsFinished === clients.count) {
+					console.log("All clients ended");
+					endTest();
+				}
+			}
+		});
+		
+		
+	}
+};
+
+
+function startTest() {
+	
+	for (var i = 0; i < clients.count; i++) {
+		clients.list[i].send(JSON.stringify({
+			"type": "startTest"
+			
+		}));
+	}
+		
+};
+
+function endTest() {
+	
+	var sum = 0;
+	for(var i =0; i<clients.avg.length; i++) {
+		
+		sum += clients.avg[i];
+	}
+	
+	var avg = sum/clients.avg.length;
+				
+	console.log("AVG: " + avg);
+
+	process.exit(0);
+};
+
+
+createClients();
